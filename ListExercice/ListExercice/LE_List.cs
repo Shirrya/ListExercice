ce127883aec7322f1347e78af1021cb27913421a
Shirrya <61795700+Shirrya@users.noreply.github.com>
2020-09-28 09:30:25 +0200

    2-Added 2 methods
    Add();
    RemoveAt();

---
+++ ListExercice/ListExercice/LE_List.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListExercice
+{
+    public class LE_List<T>
+    {
+        T[] myList = new T[0];
+        public T this[int i] => myList[i];
+
+        public int Count => myList.Length;
+
+        public void Clear() => myList = new T[0];
+
+        public void Add(T _toAdd)
+        {
+            T[] _temp = new T[Count + 1];
+
+            for (int i = 0; i < Count; i++)
+            {
+                _temp[i] = myList[i];
+            }
+            _temp[Count] = _toAdd;
+            myList = _temp;
+        }
+
+        public void RemoveAt(int _index)
+        {
+            if (_index > Count - 1)
+            {
+                Console.WriteLine($"Index {_index} is out of range, can't remove from outside the list");
+                return;
+            }
+
+            T[] _temp = new T[Count - 1];
+            for (int i = 0; i < _index; i++)
+            {
+                _temp[i] = myList[i];
+            }
+            for (int i = _index+1; i < Count; i++)
+            {
+                _temp[i-1] = myList[i];
+            }
+            myList = _temp;
+        }
+    }
+}
--- ListExercice/ListExercice/Program.cs
+++ ListExercice/ListExercice/Program.cs
@@ -10,6 +10,19 @@ namespace ListExercice
     {
         static void Main(string[] args)
         {
+            LE_List<int> _myList = new LE_List<int>();
+
+            _myList.Add(4);
+            _myList.Add(3);
+            _myList.Add(2);
+            _myList.Add(1);
+            _myList.RemoveAt(5);
+
+            for (int i = 0; i < _myList.Count; i++)
+            {
+                Console.WriteLine( _myList[i]);               
+            }
+            Console.ReadLine();
         }
     }
 }
